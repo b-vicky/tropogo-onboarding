254ef30ed8bbc68929d3a3c2c12532c5ec021a86
Vignesh B <vignesh@waitwhile.com>
2020-09-12 18:30:19 +0530

    routing and header component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { InstitueDetailsComponent } from './institue-details/institue-details.component';
 import { CourseDetailsComponent } from './course-details/course-details.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InstitueDetailsComponent,
-    CourseDetailsComponent
+    CourseDetailsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
